879fea0176db2b7706f9571960201bb3631f78b9
Louis laptop <louis.milhaud@tutamail.com>
2025-03-28 13:05:33 +0100

    src initial commit

---
+++ src/StreamGraph.py
@@ -0,0 +1,8 @@
+class StreamGraph():
+    pass
+
+    # TODO
+    # - class basics
+    # - discretization
+    # - alpha and omega functions
+    # - temporal neighbourhood
---
+++ src/utils.py
@@ -0,0 +1,2 @@
+
+# define here h functions
